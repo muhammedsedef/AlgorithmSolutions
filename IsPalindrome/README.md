35b4dbfb175b4280f9aff116d7960b55e96707cc
muhammedsedef <muhammetsedef34@gmail.com>
2022-03-10 00:03:44 +0300

    finished palindrome

---
+++ IsPalindrome/README.md
@@ -0,0 +1,7 @@
+## 02 - Palindrome Algorithm Problem ##
+
+## What is Palindrome Number? ##
+Palindromic numbers are numbers that have the same reading direction when read from both sides.
+
+Examples: 1, 4, 8, 99, 101, 363, 4004, 9889....
+
---
+++ IsPalindrome/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPalindrome(int number) {
+        int tempNumber = number, reverseNumber = 0, lastNumber;
+        while (tempNumber != 0) {
+            lastNumber = tempNumber % 10;
+            System.out.println("----------");
+            System.out.println("Last number: " + lastNumber);
+
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            System.out.println("Reverse number: " + reverseNumber);
+
+            tempNumber /= 10;
+            System.out.println("temp number: " + tempNumber);
+        }
+
+        if(number == reverseNumber)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int number = scan.nextInt();
+
+        System.out.println(isPalindrome(number));
+    }
+}
