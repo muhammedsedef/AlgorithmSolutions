00dbf44a9f756d27cb553a49162ef3265fc8d870
muhammedsedef <muhammetsedef34@gmail.com>
2022-03-09 22:19:44 +0300

    finished fibonacci series algorithm

---
+++ Fibonacci/README.md
@@ -0,0 +1,10 @@
+## 04 - Fibonacci Algorithm Problem ##
+What is the Fibonacci Series?
+
+The Fibonacci series is a sequence of numbers formed by adding each number with the previous one. 
+In this series, which continues in this way, the golden ratio emerges when the numbers are proportional to each other, 
+that is, when a number is divided by the number before it, a series that approaches the golden ratio is obtained.
+
+The Fibonacci sequence starts from 0 and goes on to infinity. 
+Each digit is added up with the previous digit. 
+The result obtained is written on the right side of the figure.
---
+++ Fibonacci/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen bir sayı giriniz: ");
+        int number = scan.nextInt();
+
+        int first = 0;
+        int second = 1;
+        int third;
+        // 0 1 1 2 3 5 8 13 21 34
+        System.out.print(first + " " + second);
+
+        for(int i = 2; i<number; i++){
+            third = first + second;
+            System.out.print(" " + third);
+            first = second;
+            second = third;
+        }
+    }
+}
